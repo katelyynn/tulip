29fe619fa37d1e2b6b12e4bf7b1bed2eca1e67ea
katelyn <46572320+katelyynn@users.noreply.github.com>
2025-04-17 15:46:10 +0100

    chore: commit

---
+++ src/main/java/org/cutensilly/tulip/TulipClient.java
@@ -0,0 +1,11 @@
+package org.cutensilly.tulip;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TulipClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
