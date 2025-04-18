511651bf4fbe11696651a91f0dd087da2de1a09b
katelyn <46572320+katelyynn@users.noreply.github.com>
2025-04-18 02:58:24 +0100

    fix: server

--- src/main/java/org/cutensilly/tulip/Tulip.java
+++ src/main/java/org/cutensilly/tulip/Tulip.java
@@ -2,11 +2,6 @@ package org.cutensilly.tulip;
 
 import net.fabricmc.api.ModInitializer;
 
-import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
-import net.minecraft.text.MutableText;
-import net.minecraft.text.Style;
-import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
 import org.cutensilly.tulip.component.TulipDataComponentTypes;
 import org.cutensilly.tulip.item.TulipItemGroups;
 import org.cutensilly.tulip.item.TulipItems;
@@ -27,33 +22,5 @@ public class Tulip implements ModInitializer {
 		TulipItemGroups.registerItemGroups();
 
 		TulipDataComponentTypes.registerDataComponentTypes();
-
-		// TODO: once docs are updated, move this to component
-		ItemTooltipCallback.EVENT.register(((itemStack, tooltipContext, tooltipType, list) -> {
-			boolean hasRarity = itemStack.contains(TulipDataComponentTypes.RARITY);
-
-			if (!hasRarity) {
-				itemStack.set(TulipDataComponentTypes.RARITY, "common");
-			}
-
-			var rarity = itemStack.get(TulipDataComponentTypes.RARITY);
-
-			assert rarity != null;
-			int rarityColour = switch (rarity) {
-				case "uncommon" -> 9089377;
-				case "rare" -> 6852573;
-				case "epic" -> 8939995;
-				case "legendary" -> 14721100;
-				case "mythic" -> 15231451;
-				case "special" -> 14374770;
-				default -> 9933455;
-			};
-
-			MutableText mutable = Text.translatable(String.format("rarity.tulip.%s", rarity))
-					.withColor(rarityColour);
-
-			list.add(Text.literal(""));
-			list.add(mutable);
-		}));
 	}
 }
